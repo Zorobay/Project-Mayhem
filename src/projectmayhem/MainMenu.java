764c107d721566eb1cc50f1750e31f4feed54c53
Adam Källberg <ad.kallberg@gmail.com>
2012-09-20 10:52:22 +0200

    fixed stuff

--- src/projectmayhem/MainMenu.java
+++ src/projectmayhem/MainMenu.java
@@ -13,22 +13,23 @@ public class MainMenu extends BasicGameState{
 
 	public static int ID;
 
-	int playButtonState;	
+		
 	Button play;
-	
+		
+	Button settings;
 	
 	public MainMenu(int ID){
 		this.ID = ID;
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-		playButtonState = 0;
-		play = new Button(new Image("graphics/buttons/playbutton.png"), new Image("graphics/buttons/playbuttonhover.png"), Button.LEFTTOP(), Button.LEFTTOP(), gc);
-		play = new Button(new Image("graphics/buttons/textsettingsbutton.png"), new Image("graphics/buttons/textsettingsbuttonhover.png"), Button.MID(), 200, Button.MID(), 0, gc);
+		play = new Button(new Image("graphics/buttons/playbutton.png"), new Image("graphics/buttons/playbuttonhover.png"), Button.MID(), Button.MID(), gc);
+		settings = new Button(new Image("graphics/buttons/textsettingsbutton.png"), new Image("graphics/buttons/textsettingsbuttonhover.png"), Button.MID(), 0, Button.MID(), 90, gc);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		play.getGraphics().draw(play.getX(), play.getY());
+		settings.getGraphics().draw(settings.getX(), settings.getY());
 		
 	}
 	
